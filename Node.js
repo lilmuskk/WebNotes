1036e6a4eaeaeb8e60b016ead2d016f3f8018050
nln <142864500+lilmuskk@users.noreply.github.com>
2025-03-28 13:58:55 +0700

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,18 +1,33 @@
-const { Sequelize } = require("sequelize");
+const express = require('express');
+const { Sequelize } = require('sequelize');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
 
 const sequelize = new Sequelize("tugas-tcc", "root", "", {
-  host: "localhost",
+  host: "34.60.241.181",
   dialect: "mysql",
 });
+
 
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log("Database connected successfully!");
+    console.log("Database connected!");
   } catch (error) {
     console.error("Database connection failed:", error);
   }
 }
 
-testConnection();
 
+app.get('/', (req, res) => {
+  res.send('Server is running! Database connection: ' + (sequelize ? 'Active' : 'Inactive'));
+});
+
+
+app.listen(PORT, '0.0.0.0', async () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  await testConnection(); 
+});
